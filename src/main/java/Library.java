0d989f691011ac5c4971bade14fd38786670fb58
Andrew Reitz <aj.reitz@gmail.com>
2014-11-18 21:10:59 -0600

    Finishing up first implementation

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,8 +2,7 @@
  * Copyright (c) 2014 Ink Applications, LLC.
  * Distributed under the MIT License (http://opensource.org/licenses/MIT)
  */
+
+/** Project files go here. */
 public class Library {
-  public boolean someLibraryMethod() {
-    return true;
-  }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -2,5 +2,7 @@
  * Copyright (c) 2014 Ink Applications, LLC.
  * Distributed under the MIT License (http://opensource.org/licenses/MIT)
  */
+
+/** Tests go in this package. */
 public class LibraryTest {
 }
