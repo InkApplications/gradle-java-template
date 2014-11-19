6d6cd6105fd43c98749ace86b57748db60fe3ed7
Andrew Reitz <aj.reitz@gmail.com>
2014-11-18 20:26:06 -0600

    Fixed checkstyles plugins

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,5 +1,9 @@
+/*
+ * Copyright (c) 2014 Ink Applications, LLC.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
-    }
+  public boolean someLibraryMethod() {
+    return true;
+  }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2014 Ink Applications, LLC.
  * Distributed under the MIT License (http://opensource.org/licenses/MIT)
  */
